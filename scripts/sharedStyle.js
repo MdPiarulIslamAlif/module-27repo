6a32432670c464ea86e1b0a60822d7d440746f99
MdPiarulIslamAlif <mdpiarulislamalif7@gmail.com>
2023-08-18 09:13:31 +0600

    added a ellipe math.pi value set and calculate

--- scripts/sharedStyle.js
+++ scripts/sharedStyle.js
@@ -10,18 +10,29 @@ function getINputValue(inputFiled){
      return inputValue;
 
 }
+// set value area
+function setValueGetElement(ElementId, area){
+     const element = document.getElementById(ElementId);
+     element.innerText = area
+}
+
+// ellipse function find out
+function ellipseArea(){
+     const ellipseLarge = getINputValue('ellipse-large');
+     const ellipseShort = getINputValue('ellipse-short');
+     const area = 3.14 * ellipseLarge * ellipseShort;
+     setValueGetElement('ellipse-Area', area)
+     
+}
 
 function parallelogramArea(){
 
    const base = getINputValue('parallelogram-base');
      
    const height = getINputValue('parallelogram-Height');
-   
-   const totalValue = base * height;
-   
-   const parallelogramArea = document.getElementById('parallelogram-Area');
-   parallelogramArea.innerText = totalValue
 
-     
+   const area = base * height;
+   
+   setValueGetElement('parallelogram-Area', area);
 
 }
