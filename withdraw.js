7e580e84e5d6f8707e7fb356075becb09948c911
MdPiarulIslamAlif <mdpiarulislamalif7@gmail.com>
2023-08-18 07:02:45 +0600

    added somen file  and image file

---
+++ withdraw.js
@@ -0,0 +1,20 @@
+// / withdraw button function addEventListener start form here
+
+// document.getElementById('btn-withdraw').addEventListener('click',function(){
+
+//      // withdraw sum function start from here
+//      const withdrawField = getInputValueById('withdraw-field');
+     
+//      const myWithdrawTotal = getInnerTextValueById('my-withdraw');
+//      const myCurrentWithdraw = withdrawField + myWithdrawTotal;
+   
+//      setTextElementValueById('my-withdraw', myCurrentWithdraw);
+//      // withdraw sum function end from here
+
+//      // withdraw submission main balance start form here
+//      const totalMainBalance = getInnerTextValueById('total-Balance');
+//      const myCurrentMainBalance = totalMainBalance - withdrawField; 
+//      setTextElementValueById('total-Balance', myCurrentMainBalance);
+     
+//      // withdraw submission main balance end form here
+// })
